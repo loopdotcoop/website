302ce6f5a82dd673db7f464c3d66380b068e9a8f
richplastow <rich@richplastow.com>
2015-10-09 18:46:53 +0100

    0.1.0 Style buttons on front-page; Responsive nav

--- wp-content/themes/ldc/header.php
+++ wp-content/themes/ldc/header.php
@@ -5,78 +5,94 @@
 <!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
 <!--[if gt IE 8]><!--> <html <?php language_attributes(); ?> class="no-js"><!--<![endif]-->
 
-	<head>
-		<meta charset="utf-8">
+  <head>
+    <meta charset="utf-8">
 
-		<?php // force Internet Explorer to use the latest rendering engine available ?>
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <?php // force Internet Explorer to use the latest rendering engine available ?>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-		<title><?php wp_title(''); ?></title>
+    <title><?php wp_title(''); ?></title>
 
-		<?php // mobile meta (hooray!) ?>
-		<meta name="HandheldFriendly" content="True">
-		<meta name="MobileOptimized" content="320">
-		<meta name="viewport" content="width=device-width, initial-scale=1"/>
+    <?php // mobile meta (hooray!) ?>
+    <meta name="HandheldFriendly" content="True">
+    <meta name="MobileOptimized" content="320">
+    <meta name="viewport" content="width=device-width, initial-scale=1"/>
 
-		<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-		<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
-		<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
-		<!--[if IE]>
-			<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-		<![endif]-->
-		<?php // or, set /favicon.ico for IE10 win ?>
-		<meta name="msapplication-TileColor" content="#f01d4f">
-		<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+    <?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
+    <link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-touch-icon.png">
+    <link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+    <!--[if IE]>
+      <link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
+    <![endif]-->
+    <?php // or, set /favicon.ico for IE10 win ?>
+    <meta name="msapplication-TileColor" content="#f01d4f">
+    <meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
             <meta name="theme-color" content="#121212">
 
-		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
+    <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
-		<?php // wordpress head functions ?>
-		<?php wp_head(); ?>
-		<?php // end of wordpress head ?>
+    <?php // wordpress head functions ?>
+    <?php wp_head(); ?>
+    <?php // end of wordpress head ?>
 
-		<?php // drop Google Analytics Here ?>
-		<?php // end analytics ?>
+    <?php // drop Google Analytics Here ?>
+    <?php // end analytics ?>
 
-	</head>
+  </head>
 
-	<body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
+  <body <?php body_class(); ?> itemscope itemtype="http://schema.org/WebPage">
 
     <div class="ldc-bkgnd"></div><!-- container for the background image -->
 
     <div id="container">
 
-			<header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
+      <header class="header" role="banner" itemscope itemtype="http://schema.org/WPHeader">
 
-				<div id="inner-header" class="wrap cf">
+        <div id="inner-header" class="wrap cf">
 
-					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
+          <?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
           <?php //// 20150921^RP commented out the default Bones logo:
 // echo '<p id="logo" class="h1" itemscope itemtype="http://schema.org/Organization"><a href="' . home_url() . '" rel="nofollow">';
 // bloginfo('name');
 // echo '</a></p>';
           ?>
 
-					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
+          <?php // if you'd like to use the site description you can un-comment it below ?>
+          <?php // bloginfo('description'); ?>
 
-					<nav role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
-						<?php wp_nav_menu(array(
-			         'container' => false,                           // remove nav container
-			         'container_class' => 'menu cf',                 // class of container (should you choose to use it)
-			         'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
-			         'menu_class' => 'nav top-nav cf',               // adding custom nav class
-			         'theme_location' => 'main-nav',                 // where it's located in the theme
-			         'before' => '',                                 // before the menu
-			               'after' => '',                                  // after the menu
-			               'link_before' => '',                            // before each link
-			               'link_after' => '',                             // after each link
-			               'depth' => 0,                                   // limit the depth of the nav
-			         'fallback_cb' => ''                             // fallback function (if there is one)
-						)); ?>
+          <nav id="ldc-nav" role="navigation" itemscope itemtype="http://schema.org/SiteNavigationElement">
+            <?php wp_nav_menu(array(
+               'container' => false,                           // remove nav container
+               'container_class' => 'menu cf',                 // class of container (should you choose to use it)
+               'menu' => __( 'The Main Menu', 'bonestheme' ),  // nav name
+               'menu_class' => 'nav top-nav cf',               // adding custom nav class
+               'theme_location' => 'main-nav',                 // where it's located in the theme
+               'before' => '',                                 // before the menu
+                     'after' => '',                                  // after the menu
+                     'link_before' => '',                            // before each link
+                     'link_after' => '',                             // after each link
+                     'depth' => 0,                                   // limit the depth of the nav
+               'fallback_cb' => ''                             // fallback function (if there is one)
+            )); ?>
 
-					</nav>
+          <!-- 20151009^RP Enable show/hide menu button -->
+          <script type="text/javascript">
+            var
+              $$collapseButtons = document.getElementsByClassName('ldc-collapse')
+             ,$collapseButton   = $$collapseButtons[0]
+             ,$nav              = document.getElementById('ldc-nav')
+            ;
+            if ($nav && $collapseButton) {
+              $collapseButton.addEventListener('click', function (event) {
+                event.preventDefault();
+                $nav.className = 'active' == $nav.className ? '' : 'active';
+              });
+            }
+          </script>
 
-				</div>
 
-			</header>
+          </nav>
+
+        </div>
+
+      </header>
